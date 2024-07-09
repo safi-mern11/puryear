0b6e142466cf9d586331573c3536546289c8812d
saifi1212 <safiullah123fix2@gmail.com>
2024-07-09 16:43:23 +0500

    second commit

--- src/app/clym/Clym.jsx
+++ src/app/clym/Clym.jsx
@@ -18,7 +18,7 @@ const Clym = () => {
             js.src='https://widget.clym-sdk.net/clym.js';
             js.onload=function(){Clym&&Clym.load(i,w,o);};
             cjs.parentNode.insertBefore(js, cjs);
-          }(document,'script','clym-privacy','3e7caf03a90b42d9afcd3bdaq01bkiqc',{}));
+          }(document,'script','clym-privacy','d9409c1d223a41afb3a63791w7bzxpfc',{}));
         `;
     document.head.appendChild(script2);
 
